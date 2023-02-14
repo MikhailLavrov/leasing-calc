6e96f64a2600abe674b948841a8caea132113ae3
Mikhail Lavrov <lavrov.fd@gmail.com>
2023-02-14 21:05:31 +0400

    initial styles on mpbile

--- src/js/calculations.js
+++ src/js/calculations.js
@@ -13,13 +13,11 @@ export const calculatePercent = () => {
   depositPercent.textContent = ((unformatValue(depositDisplay.value) / unformatValue(priceDisplay.value)) * 100).toFixed(0) + '%';
 }
 
-// *
 export const getMounthlyPayment = () => {
   monthlyPayment.value = Math.round((unformatValue(priceDisplay.value) - unformatValue(depositDisplay.value)) * (0.05 * Math.pow((1 + 0.05), unformatValue(creditTimeDisplay.value)) / (Math.pow((1 + 0.05), unformatValue(creditTimeDisplay.value)) - 1)));
   monthlyPayment.value = getFormattedValue(monthlyPayment.value);
 }
 
-// *
 export const getDealAmount = () => {
   dealAmount.value = unformatValue(depositDisplay.value) + (unformatValue(creditTimeDisplay.value) * unformatValue(monthlyPayment.value));
   dealAmount.value = getFormattedValue(dealAmount.value)
